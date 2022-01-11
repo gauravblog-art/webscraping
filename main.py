f124d764f9c0ce6a45b85cae57a3aa6fc1f052bb
Gaurav kumar <76816526+gauravblog-art@users.noreply.github.com>
2022-01-11 19:33:35 +0530

    tag identifing

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+from bs4.element import Tag
 import requests
+
 
 from bs4 import BeautifulSoup
 
@@ -8,6 +10,42 @@ r=requests.get(url)
 
 # print(content)
 
+
+# step 1
 htmlcontent=r.content
 
-print(htmlcontent)
+# print(htmlcontent)
+
+# srep 2 passe the html
+
+soup=BeautifulSoup(htmlcontent, 'html.parser')
+
+# print(soup.prettify())
+
+# step 3 HTML tree traverser
+title=soup.title
+
+print(type(title))
+
+# types of object
+
+# 1.tag
+# 2.Navigation
+# 3.beautfulshoup
+# 4.comment
+
+# print(type(soup))
+
+# print(type(title))
+# print(type(title.string)) 
+
+# 1. get all paragraph
+
+par=soup.find_all('p')
+
+print(par.priti)
+
+# 1.get all ancher Tag
+acn= soup.find_all('a')
+
+print(acn.prettify())
